bfd1b44823b6197fc7677806b1dbfdd78332f4b8
einav242 <93386470+einav242@users.noreply.github.com>
2022-05-03 09:48:00 +0300

    Add files via upload

---
+++ stack.hpp
@@ -0,0 +1,23 @@
+
+#include<string>
+#include <sys/mman.h>
+#define SIZE 1024
+using namespace std;
+
+typedef struct Node {
+    char data[SIZE];
+    struct Node *next;
+}Node; 
+
+// typedef struct mystack
+// {
+//    Node* top;
+//    int size; 
+// }mystack;
+
+
+
+void pop();
+void push(char* value);
+char* Top();
+
